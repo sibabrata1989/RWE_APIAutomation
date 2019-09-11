4afdba81a5d1862b4f5cc50aeb2fd1baf6a97e6d
Sibabrata Swain <sibabrata.swain1@gmail.com>
2019-09-11 20:53:37 +0530

    first commit

---
+++ src/test/java/LibraryFile/ConstantVaribales.java
@@ -0,0 +1,11 @@
+package LibraryFile;
+
+public interface ConstantVaribales {
+		//Response code..
+		public static final int STATUS_OK = 200;
+		public static final int STATUS_CREATED = 201;
+		public static final int STATUS_NOTFOUND = 404;
+		public static final int STATUS_BADREQUEST = 400;
+		public static final int STATUS_INTERNALSERVERERROR = 500;
+		public static final int STATUS_UNAUTHORIZED = 403;
+}
