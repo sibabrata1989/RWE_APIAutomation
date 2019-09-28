8157cdb27e5d5d3177f684ee46444d5e89dc0022
Sibabrata Swain <sibabrata.swain1@gmail.com>
2019-09-28 08:39:03 +0530

    headless chrome

--- src/test/java/LibraryFile/GenerateCookies.java
+++ src/test/java/LibraryFile/GenerateCookies.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import LibraryFile.ReusableMethodsClass;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;	
 
 public class GenerateCookies implements ConstantVaribales {
 	
@@ -19,16 +20,16 @@ public class GenerateCookies implements ConstantVaribales {
 	try
 	{
 		prp = ReusableMethodsClass.getPropertyFile();
-		System.setProperty("webdriver.chrome.driver", ".\\Drivers\\chromedriver_2.29_win32\\chromedriver.exe");
-		driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", ".\\Drivers\\chromedriver_2.29_win32\\chromedriver.exe");
+		driver = new HtmlUnitDriver();
 		
 		driver.get(prp.getProperty("QA_ENV"));
 		
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
-		driver.findElement(By.xpath("//*[@id='ctl00_PageContentPlaceholder_UserNameInput']")).sendKeys("USERNAME");
-		driver.findElement(By.xpath("//*[@id='ctl00_PageContentPlaceholder_PasswordInput']")).sendKeys("PASSWORD");
+		driver.findElement(By.xpath("//*[@id='ctl00_PageContentPlaceholder_UserNameInput']")).sendKeys(prp.getProperty("USERNAME"));
+		driver.findElement(By.xpath("//*[@id='ctl00_PageContentPlaceholder_PasswordInput']")).sendKeys(prp.getProperty("PASSWORD"));
 		driver.findElement(By.xpath("//*[@id='ctl00_PageContentPlaceholder_LoginButton']")).click();
 		Thread.sleep(3000);
 		cookies = driver.manage().getCookieNamed(COOKIES_NAME);
